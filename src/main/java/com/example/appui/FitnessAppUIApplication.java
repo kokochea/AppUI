65b58cf7f17f3a5ea45e86def5e129e3cb544f94
German <ggoicoechea@correo.um.edu.uy>
2022-09-27 15:03:20 -0300

    FitnessAppUI

--- src/main/java/com/example/appui/FitnessAppUIApplication.java
+++ src/main/java/com/example/appui/FitnessAppUIApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -19,7 +20,9 @@ public class FitnessAppUIApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("appUI.fxml"));
         Parent root = loader.load();
+        Image logo = new Image(getClass().getResourceAsStream("fitness.png"));
         Scene scene = new Scene(root);
+        stage.getIcons().add(logo);
         stage.setTitle("FitnessApp");
         stage.setScene(scene);
         stage.show();
